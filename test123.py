62152dcc27017a7baeca4fe177979d8fc309e12a
Surf1se <Surfase.an@bk.ru>
2025-02-28 16:45:21 +0300

    lesson1

--- test123.py
+++ test123.py
@@ -1,2 +1,33 @@
+user  = ["1", "2", "3", "4"]
 
-print("hi )))")
+name = "Dasha"
+
+person = {
+    "name" : name,
+    "age" : 22,
+    "isAdmin" : True,
+    "registration_adres" : {
+        "city" : "Moscow",
+        "street" : "Lybyanka",
+        "house" : 5
+    }
+}
+
+# person["name"] = "Marina"
+
+
+"""
+remove - удаляем из массива данные
+append - добавляем в массив данные 
+
+"""
+
+user.append("123123")
+user.remove("1")
+
+
+
+print (len(user))
+print (user)
+print (person)
+print (person["registration_adres"]["city"])
